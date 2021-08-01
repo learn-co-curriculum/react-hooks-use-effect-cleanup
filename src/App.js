03d1947ed838550b02ac329bdd46e8870ad6820e
GE47 <hassan2pac47@gmail.com>
2021-08-01 14:22:20 +0300

    update clock toggle
    update the button to toggle the `Clock` component just like in README.md

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
+import Clock from "./Clock";
+
 function App() {
   const [showClock, setShowClock] = useState(true);
 
   return (
     <div>
       {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(false)}>Toggle Clock</button>
+      <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
     </div>
   );
 }
